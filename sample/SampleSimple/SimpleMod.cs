98691bacfed290a09baad478fecba38f3f87486d
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2025-02-03 22:25:16 +0800

    fix: do not copy output files

--- sample/SampleSimple/SimpleMod.cs
+++ sample/SampleSimple/SimpleMod.cs
@@ -1,15 +1,22 @@
 ﻿using LibraryMod;
+using ModCore.Events.Interfaces.Game;
 using ModCore.Mods;
 
 namespace SampleSimple
 {
-    public class SimpleMod(ModInfo info) : ModBase(info)
+    public class SimpleMod(ModInfo info) : ModBase(info),
+        IOnGameExit
     {
         public override void Initialize()
         {
             Logger.Information("Hello, World!");
 
             HelloWorld.Say();
+        }
+
+        void IOnGameExit.OnGameExit()
+        {
+            Logger.Information("Game is exit");
         }
     }
 }
