ee8f666ce54e01eec09194d7f0cf3fbebb14526a
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2026-01-25 10:19:00 +0800

    feat: allow editing of mod previews

--- mdk/DCCMTool/Commands/Steam/UploadModCommnand.cs
+++ mdk/DCCMTool/Commands/Steam/UploadModCommnand.cs
@@ -20,7 +20,7 @@ namespace DCCMTool.Commands.Steam
 
             Console.WriteLine($"Finding mod {modname} in workshop...");
 
-            var handle = SteamUGC.CreateQueryUserUGCRequest(SteamUser.GetSteamID().GetAccountID() , EUserUGCList.k_EUserUGCList_Published,
+            var handle = SteamUGC.CreateQueryUserUGCRequest(SteamUser.GetSteamID().GetAccountID(), EUserUGCList.k_EUserUGCList_Published,
                 EUGCMatchingUGCType.k_EUGCMatchingUGCType_All, EUserUGCListSortOrder.k_EUserUGCListSortOrder_LastUpdatedDesc,
                 APPID, APPID, 1);
 
@@ -38,9 +38,9 @@ namespace DCCMTool.Commands.Steam
                 Console.Error.WriteLine("You appear to have uploaded multiple mods with the same name.");
                 return -1;
             }
-            else if(resultCount == 1)
+            else if (resultCount == 1)
             {
-                if(!SteamUGC.GetQueryUGCResult(handle, 0, out var details))
+                if (!SteamUGC.GetQueryUGCResult(handle, 0, out var details))
                 {
                     Console.Error.WriteLine("Failed to GetQueryUGCResult");
                     SteamUGC.ReleaseQueryUGCRequest(handle);
@@ -50,12 +50,12 @@ namespace DCCMTool.Commands.Steam
                 publishId = details.m_nPublishedFileId;
                 updateHandle = SteamUGC.StartItemUpdate(APPID, details.m_nPublishedFileId);
             }
-            else if(resultCount == 0)
+            else if (resultCount == 0)
             {
                 SteamUGC.ReleaseQueryUGCRequest(handle);
                 Console.WriteLine("Creating new item...");
                 var r = await SteamUGC.CreateItem(APPID, EWorkshopFileType.k_EWorkshopFileTypeCommunity).Wait<CreateItemResult_t>();
-                if(r.m_eResult != EResult.k_EResultOK)
+                if (r.m_eResult != EResult.k_EResultOK)
                 {
                     Console.WriteLine("Failed to create new item: " + r.m_eResult.ToString());
                     return -3;
@@ -70,13 +70,14 @@ namespace DCCMTool.Commands.Steam
             SteamUGC.AddItemKeyValueTag(updateHandle, "dccm_modver", ver);
             SteamUGC.AddItemKeyValueTag(updateHandle, "dccm_modname", modname);
 
+            if (string.IsNullOrEmpty(Arguments.PreviewPath))
             {
                 string[] previewExt = ["jpg", "png", "gif"];
                 var previewPath = Path.Combine(Arguments.ModPath, "preview.");
-                foreach(var v in previewExt)
+                foreach (var v in previewExt)
                 {
                     var rp = previewPath + v;
-                    if(File.Exists(rp))
+                    if (File.Exists(rp))
                     {
                         Console.WriteLine("Found preview: " + rp);
                         SteamUGC.SetItemPreview(updateHandle, rp);
@@ -84,17 +85,28 @@ namespace DCCMTool.Commands.Steam
                     }
                 }
             }
+            else
+            {
+                SteamUGC.SetItemPreview(updateHandle, Arguments.PreviewPath);
+            }
 
             SteamUGC.SetItemContent(updateHandle, Arguments.ModPath);
 
-            if(string.IsNullOrEmpty(Arguments.UpdateText))
+            if (string.IsNullOrEmpty(Arguments.UpdateText))
             {
                 Arguments.UpdateText = "Update to v" + ver;
             }
 
             Console.WriteLine("Uploading...");
 
-            SteamUGC.SubmitItemUpdate(updateHandle, Arguments.UpdateText);
+            var sresult = await SteamUGC.SubmitItemUpdate(updateHandle, Arguments.UpdateText).Wait<SubmitItemUpdateResult_t>();
+            
+            if(sresult.m_eResult != EResult.k_EResultOK)
+            {
+                Console.WriteLine("Unable to upload mod： " + sresult.m_eResult);
+                Console.WriteLine("Please visit https://partner.steamgames.com/doc/api/ISteamUGC#SubmitItemUpdateResult_t for more information.");
+                return -1;
+            }
 
             Console.WriteLine("Mod Workshop Id: " + publishId.m_PublishedFileId);
             Console.WriteLine("You can access your mod here.: https://steamcommunity.com/sharedfiles/filedetails/?id=" + publishId.m_PublishedFileId);
@@ -107,10 +119,12 @@ namespace DCCMTool.Commands.Steam
         [Verb("upload-mod", false, HelpText = "Upload a mod to Steam Workshop.")]
         public class Options
         {
-            [Option('i', "input", Required = true, HelpText = "Path to the mod directory to upload.")]
+            [Option('i', "input", Required = true, HelpText = "The path for the mod directory.")]
             public required string ModPath { get; set; }
             [Option('t', "update-text", HelpText = "The update text for the mod upload.")]
             public string? UpdateText { get; set; }
+            [Option('p', "preview", HelpText = "The path for the mod preview")]
+            public string? PreviewPath { get; set; }
         }
     }
 }
