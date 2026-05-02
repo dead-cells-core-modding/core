4540147151b8874c4a9e4f8f04a48d998e0ea906
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2026-05-02 13:18:04 +0800

    chore: update editorconfig

--- sources/Haxe2CSharp/HaxeCompiler.cs
+++ sources/Haxe2CSharp/HaxeCompiler.cs
@@ -1,10 +1,12 @@
 using Hashlink.Reflection;
 using Hashlink.Reflection.Members;
+using HaxeProxy.Runtime;
 using Mono.Cecil;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using TypeAttributes = Mono.Cecil.TypeAttributes;
 
 namespace Haxe2CSharp
 {
@@ -13,14 +15,33 @@ namespace Haxe2CSharp
         HashlinkModule module,
         Assembly proxy)
     {
+        private RuntimeHelperRef? runtimeHelperRef = null;
+
         public AssemblyDefinition Assembly
         {
             get;
         } = AssemblyDefinition.CreateAssembly(new(moduleName, new()), moduleName, ModuleKind.Dll);
 
+        public ModuleDefinition MainModule => Assembly.MainModule;
+
         public void Compile(HashlinkFunction func)
         {
-        
+            runtimeHelperRef ??= new(MainModule);
+
+            var dt = func.DeclaringType == null ? null : HaxeProxyUtils.GetProxyType(func.DeclaringType);
+
+            var dtNspec = dt?.Namespace;
+            var dtName = dt?.Name ?? "UnknownType";
+
+            var dtd = MainModule.GetType(dtNspec, dtName);
+
+            if (dtd == null)
+            {
+                dtd = new TypeDefinition(dtNspec, dtName, TypeAttributes.NotPublic | TypeAttributes.Sealed);
+                MainModule.Types.Add(dtd);
+            }
+
+
         }
     }
 }
